bc626865ee82c8d8645cd02f7e6bf72d18b1868d
ganiy <ganiy@cdo.center>
2017-06-25 18:19:11 +0300

    fixed some stuff

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-import traceback
 from Model.RedirectionDetecter import RedirectionDetecter
 
 
 if __name__ == '__main__':
-    try:
-        initial_url = "http://w3schools.com"
-        rd = RedirectionDetecter()
-        print rd.find_redirection_chain(initial_url)
+    initial_url = "http://w3schools.com"
+    rd = RedirectionDetecter()
+    print rd.find_redirection_chain(initial_url)
 
-        initial_url = "http://10.0.75.1/redirect1.html"
-        print rd.find_redirection_chain(initial_url)
-
-    except Exception, e:
-        print e.message
-        print traceback.format_exc()
+    initial_url = "http://10.0.75.1/redirect1.html"
+    print rd.find_redirection_chain(initial_url)
 
 
 
